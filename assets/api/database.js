4b6df1093ebb01f245763ff0b2a685de0fcb8c3b
NeiltonSeguins <seguins.neilton@gmail.com>
2022-06-15 16:43:19 -0300

    Adiciona produtos e dados ao database.js

---
+++ assets/api/database.js
@@ -0,0 +1,179 @@
+export const dados = [
+  {
+    categoria: 'Star Wars',
+    produto: [
+      {
+        id: 0,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/star wars/unsplash_6FDXGY9J6y4.png',
+        img_alt: 'Caneca Stormtropper',
+      },
+      {
+        id: 1,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/star wars/unsplash_epRFE_hBNjo.png',
+        img_alt: 'Dart Vader',
+      },
+      {
+        id: 2,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/star wars/unsplash_KeGToDVN0l4.png',
+        img_alt: 'Mestre Yoda',
+      },
+      {
+        id: 3,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/star wars/unsplash_4OHkK555s1A.png',
+        img_alt: 'Stormtropper',
+      },
+      {
+        id: 4,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/star wars/unsplash_R8L1l9RN198.png',
+        img_alt: 'Baby Yoda',
+      },
+      {
+        id: 5,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/star wars/unsplash_1VV1MRafd7A.png',
+        img_alt: 'Kylo Ren',
+      },
+    ],
+  },
+  {
+    categoria: 'Consoles',
+    produto: [
+      {
+        id: 0,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/consoles/Skill.png',
+        img_alt: 'XBox',
+      },
+      {
+        id: 1,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/consoles/unsplash_caNzzoxls8Q.png',
+        img_alt: 'Play Station 4',
+      },
+      {
+        id: 2,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/consoles/unsplash_ZV7lnfyQLmA.png',
+        img_alt: 'Nintendinho',
+      },
+      {
+        id: 3,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/consoles/unsplash_wa5z9o9fgjw.png',
+        img_alt: 'Nintendo Switch',
+      },
+      {
+        id: 4,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/consoles/unsplash_Zjn4dT993-g.png',
+        img_alt: 'X Box',
+      },
+      {
+        id: 5,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/consoles/unsplash_k-xYhI3-gJM.png',
+        img_alt: 'Gameboy',
+      },
+    ],
+  },
+  {
+    categoria: 'Diversos',
+    produto: [
+      {
+        id: 0,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/diversos/Skill.png',
+        img_alt: 'Camisa',
+      },
+      {
+        id: 1,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/diversos/unsplash_bUgaIaZysH0.png',
+        img_alt: 'Controle Switch',
+      },
+      {
+        id: 2,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/diversos/unsplash_sYVY_ZKwaxU.png',
+        img_alt: 'Sonic figure',
+      },
+      {
+        id: 3,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/diversos/unsplash_jMT6BrgBuXU.png',
+        img_alt: 'Rádio antigo cor de rosa',
+      },
+      {
+        id: 4,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/diversos/unsplash_MxVkWPiJALs.png',
+        img_alt: 'Óculos de realidade virtual',
+      },
+      {
+        id: 5,
+        titulo: 'Produto XYZ',
+        descricao:
+          'Voluptas voluptatum quibusdam similique, class debitis ali maecenas eveniet ridiculus, facilis fusce!',
+        preco: 'R$ 60,00',
+        img_url: './assets/img/produtos/diversos/unsplash_r27umXAelDc.png',
+        img_alt: 'Pikachu gigante de pelúcia',
+      },
+    ],
+  },
+];
