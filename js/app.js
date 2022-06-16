9de44ec78580e8cf7d247fa68984ef715b64c104
NeiltonSeguins <seguins.neilton@gmail.com>
2022-06-16 15:42:48 -0300

    Carrega a página inicial de forma  dinâmica

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
-function fazerLogin() {
-  const url = window.location.href;
-  const telaLogin = "http://127.0.0.1:5500/login.html";
-  if (url !== telaLogin) {
-    window.location.assign(telaLogin);
-  }
-}
+import { exibeProdutos } from './exibeProdutos.js';
+import { dados } from '../assets/api/database.js';
 
+exibeProdutos(dados);
--- js/interacoes.js
+++ js/interacoes.js
@@ -1,4 +1,12 @@
 function handleCarrinho() {
-    const carrinho = document.querySelector('.carrinho');
-    carrinho.classList.toggle('carrinho--ativo');
+  const carrinho = document.querySelector('.carrinho');
+  carrinho.classList.toggle('carrinho--ativo');
+}
+
+function fazerLogin() {
+  const url = window.location.href;
+  const telaLogin = 'http://127.0.0.1:5500/login.html';
+  if (url !== telaLogin) {
+    window.location.assign(telaLogin);
+  }
 }
