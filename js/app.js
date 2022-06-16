111aef4e46431bc0346c2466fea56ffcf387cd1e
Diego Carlos <80489469+diegocgayoso@users.noreply.github.com>
2022-06-16 17:50:13 -0300

    Revert "Branch Neilton para Diego"

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
-import { exibeProdutos } from './exibeProdutos.js';
-import { dados } from '../assets/api/database.js';
+function fazerLogin() {
+  const url = window.location.href;
+  const telaLogin = "http://127.0.0.1:5500/login.html";
+  if (url !== telaLogin) {
+    window.location.assign(telaLogin);
+  }
+}
 
-exibeProdutos(dados);
