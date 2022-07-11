2c54078c8a3ae5a6cdc08f0c901ad67cca4bf542
Neilton Seguins <77410403+NeiltonSeguins@users.noreply.github.com>
2022-07-11 11:44:14 -0300

    Corrige erro da página de login

--- js/interacoes.js
+++ js/interacoes.js
@@ -5,7 +5,7 @@ function handleCarrinho() {
 
 function fazerLogin() {
   const url = window.location.href;
-  const telaLogin = 'http://127.0.0.1:5500/login.html';
+  const telaLogin = 'https://challend-frontend-alura.vercel.app/login.html';
   if (url !== telaLogin) {
     window.location.assign(telaLogin);
   }
